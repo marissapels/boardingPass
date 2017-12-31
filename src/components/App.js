7796eca3e61e6abbb847e48d7038a42583cb3417
Marissa Pels <marissapels@gmail.com>
2017-12-31 15:14:45 -0600

    webpack runs, initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+
+export class App extends Component {
+  render(){
+    return (
+      <div className="app">
+        <h1>Boarding Pass</h1>
+        <div>This is the homepage!</div>
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { App } from './components/App'
+import { Router, Route, hashHistory } from 'react-router'
+
+window.React = React
+
+render(
+<Router history={hashHistory}>
+  <Route path="/" component={App}/>
+  </Router>,
+  document.getElementById('react-container')
+)
+
